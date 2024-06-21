e36f37db9ad4356ba7fb24a2261653cc043b54e2
luv <luvbhayana4623@gmail.com>
2024-06-21 17:57:05 +0530

    first

--- src/main/java/com/food/model/Food.java
+++ src/main/java/com/food/model/Food.java
@@ -1,26 +1,19 @@
 package com.food.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
-public class OrderItem {
+public class Food {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @ManyToOne
-    private Food food;
-    private int quantity;
-
-    private Long totalPrice;
-    private List<String> ingredients;
-
 }
