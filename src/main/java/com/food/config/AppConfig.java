a99877de5a2f3f6f7e5cbff1661c4e5f5a0ef4b6
luv <luvbhayana4623@gmail.com>
2024-07-02 23:43:09 +0530

    JWT

--- src/main/java/com/food/config/AppConfig.java
+++ src/main/java/com/food/config/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -51,5 +52,8 @@ public class AppConfig {
             }
         };
     }
-    PasswordEncoder
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
