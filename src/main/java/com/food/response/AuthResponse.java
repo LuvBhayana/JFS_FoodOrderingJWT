f2a0f43765a65370969af383e57c24cbddc460e8
luv <luvbhayana4623@gmail.com>
2024-07-06 17:56:05 +0530

    authenticate

---
+++ src/main/java/com/food/response/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.food.response;
+
+import com.food.model.USER_ROLE;
+import com.food.model.User;
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+    private String jwt;
+    private String message;
+
+    private USER_ROLE role;
+
+}
